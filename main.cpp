e417e563bceb4273c4cec70d8f86413cd0b9111a
BloodDeath <francuzlol2@gmail.com>
2017-03-01 16:21:56 +0100

    Poprawiono liczenie

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ int main()
     int sum=0;
     for (int i=1; i<=100; i++)
     {
-        sum=sum * i;
+        sum=sum+ (i+1) * i;
     }
     std::cout << sum << std::endl;
     std::cout << "Hello World!" << std::endl;
