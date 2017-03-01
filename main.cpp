bb0d38413f7b77067403770a97609c60c76c3898
BloodDeath <francuzlol2@gmail.com>
2017-03-01 16:07:30 +0100

    Dodano liczenie pewnej rzeczy.

--- main.cpp
+++ main.cpp
@@ -5,6 +5,12 @@
 
 int main()
 {
+    int sum=0;
+    for (int i=1; i<=100; i++)
+    {
+        sum=sum * i;
+    }
+    std::cout << sum << std::endl;
     std::cout << "Hello World!" << std::endl;
     return 0;
 }
